52ab0b0c4bce99cd28a07608c944f0ab34d10322
Leon740 <dobrinovleonid@gmail.com>
2025-02-03 10:58:33 -0500

    reg(Sun_Feb_1)

---
+++ src/js/js_4.ts
@@ -0,0 +1,38 @@
+import { log } from '@src/utils/log.ts';
+
+export function js_4() {
+  type JSONValueT = null | boolean | number | string | JSONValueT[] | { [key: string]: JSONValueT };
+
+  const getArgumentsLength = (...args: JSONValueT[]): number => {
+    console.log(args);
+    if (!args?.length) return 0;
+
+    return args.length;
+  };
+
+  const callGetArgumentsLengthWithNoArgs = getArgumentsLength();
+  log(
+    callGetArgumentsLengthWithNoArgs,
+    callGetArgumentsLengthWithNoArgs === 0,
+    'if args are not provided return 0'
+  );
+
+  const callGetArgumentsLengthWithFalsyArgs = getArgumentsLength(false, null, undefined, 0);
+  log(
+    callGetArgumentsLengthWithFalsyArgs,
+    callGetArgumentsLengthWithFalsyArgs === 4,
+    'count all falsy args'
+  );
+
+  const callGetArgumentsLengthWithAnyArgs = getArgumentsLength(
+    1,
+    0,
+    true,
+    false,
+    null,
+    undefined,
+    [],
+    {}
+  );
+  log(callGetArgumentsLengthWithAnyArgs, callGetArgumentsLengthWithAnyArgs === 8, 'count all args');
+}
